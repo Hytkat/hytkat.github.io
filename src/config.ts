bbf04912f8d6f2d041fafd666a78fb8230bd37ca
Hytkat <hytkate@gmail.com>
2025-09-07 14:46:01 +0530

    Initial commit of the project

---
+++ src/config.ts
@@ -0,0 +1,47 @@
+export const siteConfig = {
+  name: "Hirakjyoti Kalita",
+  title: "Frontend Developer",
+  description: "Portfolio website of Hirakjyoti Kalita",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "hytkat@gmail.com",
+    linkedin: "https://www.linkedin.com/in/hirak-is-a-dev/",
+    twitter: "https://x.com/hytkate",
+    github: "https://github.com/hytkat",
+  },
+  aboutMe:
+    "I'm a passionate web developer with expertise in building robust applications using Next.js, Svelte, React.js, and Node.js. My work includes a YouTube clone called Numer, a dynamic hackathon dashboard called GreenCode, and a decentralized social platform named Lumen. I thrive on building innovative solutions and am always exploring new challenges.",
+  skills: ["React", "Node.js", "Svelte", "Next.js", "MongoDB", "PostgreSQL", "Docker"],
+  projects: [
+    {
+      name: "Greencode",
+      description:
+        "A comprehensive dashboard for hackathon teams where they can sign in, create teams, and participate in different hackathons. Features team management, event tracking, and collaborative tools for hackathon participants.",
+      link: "https://github.com/Hytkat/greencode",
+      skills: ["SvelteKit", "Shadcn", "PocketBase", "TailwindCSS"],
+    },
+    {
+      name: "Numer",
+      description:
+        "A YouTube-like platform where users can share and discover videos. Features video upload, streaming, user profiles, and social interactions similar to popular video sharing platforms.",
+      link: "#",
+      skills: ["Next.js", "MongoDB", "Shadcn", "TailwindCSS"],
+    },
+    {
+      name: "Lumen",
+      description:
+        "A decentralized Reddit-like website where users can create communities, share posts, and engage in discussions. Working as a front-end developer to build an intuitive and responsive user interface.",
+      link: "https://github.com/Hytkat/lumen",
+      skills: ["SvelteKit", "Material UI", "Daisy UI"],
+    },
+  ],
+  education: [
+    {
+      school: "Assam Down Town University ",
+      degree: "Bachelor of Science in Computer Science",
+      dateRange: "2023 - 2027",
+     
+    },
+    
+  ],
+};
